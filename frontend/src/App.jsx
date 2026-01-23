7aa365a1b2ec1d9c9491c38f12612c54431342e2
Vijay R <vijayr23072008@gmail.com>
2026-01-23 18:10:03 +0530

    Hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import './App.css'
+import './Media.css'
 import Login from './components/Login'
 import TrendingNow from './components/TrendingNow'
 import FeatureComponent from './components/FeatureContainer'
