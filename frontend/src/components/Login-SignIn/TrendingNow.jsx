8b6bba6a102012b9d236a494f82f90bfccd47c44
Harivigneshr57 <harivigneshr57@gmail.com>
2026-01-27 17:39:43 +0530

    file path changes

--- frontend/src/components/Login-SignIn/TrendingNow.jsx
+++ frontend/src/components/Login-SignIn/TrendingNow.jsx
@@ -1,9 +1,9 @@
 import TrendingContainer from "./TrendingMovieContainer";
-import movie1 from "../assets/Tmovie1.png";
-import movie2 from "../assets/Tmovie2.png";
-import movie3 from "../assets/Tmovie3.png";
-import movie4 from "../assets/Tmovie4.png";
-import movie5 from "../assets/Tmovie5.png";
+import movie1 from "../../assets/Tmovie1.png";
+import movie2 from "../../assets/Tmovie2.png";
+import movie3 from "../../assets/Tmovie3.png";
+import movie4 from "../../assets/Tmovie4.png";
+import movie5 from "../../assets/Tmovie5.png";
 
 
 export default function TrendingNow(){
